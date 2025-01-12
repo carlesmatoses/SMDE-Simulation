787d4ef747d4032aef5f19febe2e7351118aed04
IG <11618271+stereoknife@users.noreply.github.com>
2025-01-12 12:42:59 +0100

    Some data analysis

--- Scripts/runners.R
+++ Scripts/runners.R
@@ -15,10 +15,10 @@ process_data <- function(file) read.csv(file) %>% mutate(
   Pace = h2s(Pace),
   Official.Time = h2s(Official.Time),
   M.F = as.factor(M.F),
-  Bib = as.integer(Bib),
   City = as.factor(City),
   State = as.factor(State),
   Country = as.factor(Country),
+  AgeRange = as.integer(Age/5) * 5
 )
 
 # 1 Read data
@@ -26,6 +26,44 @@ m15 <- process_data("./Data/marathon_results_2015.csv")
 m16 <- process_data("./Data/marathon_results_2016.csv")
 m17 <- process_data("./Data/marathon_results_2017.csv")
 
+# Time distributions (not normal)
 hist(m15$Official.Time)
 hist(m16$Official.Time)
 hist(m17$Official.Time)
+
+# Age distributions
+hist(m15$Age, breaks = 16)
+hist(m15[m15$Age < 60, "Age"], breaks = 8)
+
+# Drop runners older than 60
+m15.sub60 <- m15 %>% filter(Age >= 20 & Age < 60)
+
+# Check gender distributions
+boxplot(log(Official.Time) ~ M.F, data = m15.sub60)
+boxplot(log(Official.Time) ~ AgeRange, data = m15.sub60)
+
+boxplot(log(Official.Time) ~ AgeRange, data = m15.sub60[m15.sub60$M.F == "M", c("Official.Time", "AgeRange")])
+boxplot(log(Official.Time) ~ AgeRange, data = m15.sub60[m15.sub60$M.F == "F", c("Official.Time", "AgeRange")])
+
+# Check pace distribution
+plot(Official.Time ~ Pace, data = m15)
+hist(log(m15$Pace))
+hist(log(m15.sub60$Official.Time))
+
+# Check normality for combined
+hist(log(m15.sub60$Official.Time))
+qqnorm(log(m15.sub60$Official.Time))
+
+hist(log(m15.sub60$Pace))
+qqnorm(log(m15.sub60$Pace))
+
+# Check normality for male
+hist(log(m15.sub60[m15.sub60$M.F == "M", "Official.Time"]))
+qqnorm(log(m15.sub60[m15.sub60$M.F == "M", "Official.Time"]))
+
+# Check normality for female
+hist(log(m15.sub60[m15.sub60$M.F == "F", "Official.Time"]))
+qqnorm(log(m15.sub60[m15.sub60$M.F == "F", "Official.Time"]))
+
+summary(m15.sub60$Pace)
+sd(m15.sub60$Pace)
